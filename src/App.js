ff64a163303daf6d2dd311306b0ba82ccc36208f
Luis Tellez <luis@luistellez.com>
2022-09-21 11:56:01 -0700

    feat: update coming soon message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Website under construction using <code>React.js</code> to improve
+          experience.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <strong>Luis Tellez | Front End Web Developer</strong>
       </header>
     </div>
   );
