03c1947a6a6e26f430f201d62d84308f97d984b9
Luis Tellez <luis@luistellez.com>
2022-09-30 00:57:03 -0700

    style: define css rules for nav-bar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import './index.scss';
 import LogoL from '../../assets/images/logo-l.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
@@ -7,8 +7,14 @@ const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoL} alt='logo' />
-            <img src={LogoSubtitle} alt='luistellez' />
+            <img className='sub-logo' src={LogoSubtitle} alt='luistellez' />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <fontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+
+            </NavLink>
+        </nav>
     </div>
 )
 
