52516055a442e0d177666a55b6f9dedbf690329e
Luis Tellez <luis@luistellez.com>
2022-09-30 11:19:23 -0700

    style: adjust formatting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
-import Layout  from "./components/Layout";
 import Home from "./components/Home";
+import Layout  from "./components/Layout";
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-        </Route>
-    </Routes>
-    </>
-  );
+      <>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </>
+  )
 }
 
 export default App;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,11 @@
-const Home = () => ("Hello World");
+const Home = () => {
+    return (
+    <div className="container home-page">
+        <div className="text-zone">
+            <h1>Hi, <br/> I'm</h1>
+        </div>
+    </div>
+  );
+}
 
 export default Home;
