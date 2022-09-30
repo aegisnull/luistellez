69413f4d69680119f1dc3c2fa5f991159cbc3ac6
Luis Tellez <luis@luistellez.com>
2022-09-30 11:24:42 -0700

    feat: add h1 text for Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,15 @@
+import LogoTitle from '../../assets/images/logo-l.png';
+
 const Home = () => {
     return (
     <div className="container home-page">
         <div className="text-zone">
-            <h1>Hi, <br/> I'm</h1>
+            <h1>Hi, <br/> I'm
+            <img src={LogoTitle} alt="developer" />
+            Luis Tellez
+            <br/>
+            web developer
+            </h1>
         </div>
     </div>
   );
