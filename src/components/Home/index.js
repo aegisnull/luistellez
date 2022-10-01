f8baf49bc8890a00149b48c42dd89cd97ef7ae48
Luis Tellez <luis@luistellez.com>
2022-09-30 20:25:05 -0700

    js: replace Hi, I'm with dinamyc content

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,8 @@
 import LogoTitle from '../../assets/images/logo-l.png';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import { useState } from 'react';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -25,11 +27,23 @@ const Home = () => {
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
-          <img src={LogoTitle} alt="developer" />
-          uis Tellez
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          web developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
         <h2>Frontend Developer / UI/UX Enthusiast</h2>
         <Link to="/contact" className="flat-button">
