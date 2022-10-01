d20ae825395604a00374bb1bb0dde090179eab20
Luis Tellez <luis@luistellez.com>
2022-09-30 18:00:50 -0700

    style: use prettier styling plugin

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,25 @@
-import LogoTitle from '../../assets/images/logo-l.png';
-import {Link} from "react-router-dom";
-import './index.scss';
+import LogoTitle from '../../assets/images/logo-l.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
 
 const Home = () => {
-    return (
+  return (
     <div className="container home-page">
-        <div className="text-zone">
-            <h1>Hi, <br/> I'm
-            <img src={LogoTitle} alt="developer" />
-            Luis Tellez
-            <br/>
-            web developer
-            </h1>
-            <h2>Frontend Developer / UI/UX Enthusiast</h2>
-            <Link to="/contact" className="flat-button">CONTACT ME</Link>
-        </div>
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          Luis Tellez
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend Developer / UI/UX Enthusiast</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
     </div>
-  );
+  )
 }
 
 export default Home
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,46 +1,68 @@
-import { Link, NavLink } from "react-router-dom";
-import './index.scss';
-import LogoL from '../../assets/images/logo-l.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons';
-import {faGithub, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons";
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoL from '../../assets/images/logo-l.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoL} alt='logo' />
-            <img className='sub-logo' src={LogoSubtitle} alt='luistellez' />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/luistellezv/">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://github.com/aegisnull">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel="noreferrer" href="https://twitter.com/LuisTellezDev">
-                    <FontAwesomeIcon icon={faTwitter} color="#4d4d4e"/>
-                </a>
-            </li>
-        </ul>
-    </div>
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoL} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="luistellez" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/luistellezv/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/aegisnull">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://twitter.com/LuisTellezDev"
+        >
+          <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
 )
 
-export default Sidebar;
+export default Sidebar
