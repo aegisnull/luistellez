f5e20cdfa0e4e5bfdc1c3c12871c3c72bf37b4b1
Luis Tellez <luis@luistellez.com>
2022-10-01 17:25:22 -0700

    style: add animations to About component text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,21 @@
 import "./index.scss";
 import AnimatedLetters from "../AnimatedLetters";
+import {useEffect, useState} from "react";
 
 const About = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+    }, []);
+
     return (
         <div className="container about-page">
             <div className="text-zone">
                 <h1><AnimatedLetters
+                    letterClass={letterClass}
                 strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
                 idx={15}
                 />
