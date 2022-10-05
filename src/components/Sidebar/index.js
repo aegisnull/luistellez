f7ed34431043782ece796ce033ea4da09786ec36
Luis Tellez <luis@luistellez.com>
2022-10-05 16:12:39 -0700

    fix: fix syntax typo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -15,65 +15,71 @@ import {
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
 
-const Sidebar = () => (
-  <div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={LogoL} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="luistellez" />
-    </Link>
-    <nav>
-      <NavLink exact="true" activeclassname="active" to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-      >
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        className="contact-link"
-        to="/contact"
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-    <ul>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/luistellezv/"
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoL} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="luistellez" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
         >
-          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a target="_blank" rel="noreferrer" href="https://github.com/aegisnull">
-          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-        </a>
-      </li>
-      <li>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://twitter.com/LuisTellezDev"
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
         >
-          <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
-        </a>
-      </li>
-    </ul>
-    <FontAwesomeIcon
-      icon={faBars}
-      color="var(--secondary-color)"
-      size="3x"
-      className="hamburger-icon"
-    />
-  </div>
-);
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/luistellezv/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/aegisnull"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://twitter.com/LuisTellezDev"
+          >
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+      <FontAwesomeIcon
+        icon={faBars}
+        color="var(--secondary-color)"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  );
+};
 
 export default Sidebar;
