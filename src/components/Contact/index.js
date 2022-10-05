8f8077d04b3b58412dced25b98fb82434ef1f083
Luis Tellez <luis@luistellez.com>
2022-10-05 13:43:47 -0700

    js: add bouncing text effect to contact me title

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,30 @@
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
 
 const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+
   return (
-    <div className="contact">
-      <h1>Contact</h1>
-    </div>
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+        </div>
+      </div>
+    </>
   );
 };
 
