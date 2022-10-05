9a4b27daa12f0437572803aaee86e3c51eb41730
Luis Tellez <luis@luistellez.com>
2022-10-05 13:53:52 -0700

    feat: add HTML markup for Contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -26,6 +26,41 @@ const Contact = () => {
             I am interested in new opportunities and challenges. If you have any
             questions or want to work with me, please contact me.
           </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  ></input>
+                </li>
+                <li>
+                  <textarea
+                    name="message"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
     </>
