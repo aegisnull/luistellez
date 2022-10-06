af4c7dace8f5fdd9d8ae443c6292b3f09e59ffa9
Luis Tellez <luis@luistellez.com>
2022-10-05 23:19:02 -0700

    style: adjust Portfolio paddings

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+
   return (
-    <div className="portfolio-container">
-      <h1>Portfolio</h1>
+    <div className="container portfolio-page">
+      <h1 className="page-title">
+        <AnimatedLetters
+          letterClass={letterClass}
+          strArray={['P', 'o', 'r', 't', 'f', 'o', 'l', 'i', 'o']}
+          idx={15}
+        />
+      </h1>
     </div>
   );
 };
