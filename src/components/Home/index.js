8509375a7f235ea49b4623dc69baae4e9ec0097e
Luis Tellez <luis@luistellez.com>
2022-11-05 15:18:51 -0700

    refactor: replace absolute positioning with flexbox

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,20 @@ import Logo from './Logo';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
-  const nameArray = ['u', 'i', 's', ' ', 'T', 'e', 'l', 'l', 'e', 'z'];
+  const nameArray = [
+    ' ',
+    'L',
+    'u',
+    'i',
+    's',
+    ' ',
+    'T',
+    'e',
+    'l',
+    'l',
+    'e',
+    'z',
+  ];
   const jobArray = [
     'w',
     'e',
@@ -32,32 +45,33 @@ const Home = () => {
 
   return (
     <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Frontend Developer / UI/UX Enthusiast</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+      <div className="content">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Frontend Developer / UI/UX Enthusiast</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
     </div>
   );
 };
