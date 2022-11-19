35f1291aae3a4e9ba28bae416bfbe5dc55c3b590
Luis Tellez <luis@luistellez.com>
2022-11-18 22:53:37 -0800

    fix: comment out fixed tags on page

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,13 +7,13 @@ const Layout = () => {
     <div className="App">
       <Sidebar />
       <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
+        {/* <span className="tags top-tags">&lt;body&gt;</span> */}
         <Outlet />
-        <span className="tags bottom-tags">
+        {/* <span className="tags bottom-tags">
           &lt;/body&gt;
           <br />
           <span className="bottom-tag-html">&lt;/html&gt;</span>
-        </span>
+        </span> */}
       </div>
     </div>
   );
