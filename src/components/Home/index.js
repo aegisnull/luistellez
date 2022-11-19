34932d3c388502d8a97539d391fb128ec811d2e9
Luis Tellez <luis@luistellez.com>
2022-11-18 23:01:50 -0800

    fix: add About and Contact section to Home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,9 @@ import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 import React from 'react';
 import Logo from './Logo';
+import About from '../About';
+import Portfolio from '../Portfolio';
+import Contact from '../Contact';
 
 const Home = () => {
   const [letterClass, setLetterClass] = React.useState('text-animate');
@@ -43,35 +46,39 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container home-page">
-      <div className="content">
-        <div className="text-zone">
-          <h1>
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={nameArray}
-              idx={15}
-            />
-            <br />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={jobArray}
-              idx={22}
-            />
-          </h1>
-          <h2>Frontend Developer / UI/UX Enthusiast</h2>
-          <Link to="/contact" className="flat-button">
-            CONTACT ME
-          </Link>
+    <>
+      <div className="container home-page">
+        <div className="content">
+          <div className="text-zone">
+            <h1>
+              <span className={letterClass}>H</span>
+              <span className={`${letterClass} _12`}>i,</span>
+              <br />
+              <span className={`${letterClass} _13`}>I</span>
+              <span className={`${letterClass} _14`}>'m</span>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}
+              />
+              <br />
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={jobArray}
+                idx={22}
+              />
+            </h1>
+            <h2>Frontend Developer / UI/UX Enthusiast</h2>
+            <Link to="/contact" className="flat-button">
+              CONTACT ME
+            </Link>
+          </div>
+          <Logo />
         </div>
-        <Logo />
       </div>
-    </div>
+      <About />
+      <Contact />
+    </>
   );
 };
 
