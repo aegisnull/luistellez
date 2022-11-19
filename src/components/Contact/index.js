f190f6120608a584985d08c5ee58d5ad0325530b
Luis Tellez <luis@luistellez.com>
2022-11-19 13:15:22 -0800

    refactor: cleanup design on Contact me Section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,63 +36,58 @@ const Contact = () => {
 
   return (
     <>
-      <div className="section contact-page">
-        <div className="content">
-          <div className="text-zone">
-            <h1>
+      <section className="section contact-page">
+        <div className="contact">
+          <h1 className="contact__title">
+            <span className="underline">
               <AnimatedLetters
                 letterClass={letterClass}
                 strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
                 idx={15}
               />
-            </h1>
-            <p>
-              I am interested in new opportunities and challenges. If you have
-              any questions or want to work with me, please contact me.
-            </p>
-            <div className="contact-form">
-              <form ref={refForm} onSubmit={sendEmail}>
-                <ul>
-                  <li className="half">
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="Name"
-                      required
-                    />
-                  </li>
-                  <li className="half">
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      required
-                    />
-                  </li>
-                  <li>
-                    <input
-                      type="text"
-                      name="subject"
-                      placeholder="Subject"
-                      required
-                    ></input>
-                  </li>
-                  <li>
-                    <textarea
-                      name="message"
-                      placeholder="Message"
-                      required
-                    ></textarea>
-                  </li>
-                  <li>
-                    <input type="submit" className="flat-button" value="SEND" />
-                  </li>
-                </ul>
-              </form>
-            </div>
+            </span>
+          </h1>
+          <p className="contact__paragraph">
+            I am interested in new opportunities and challenges. If you have any
+            questions or want to work with me, please contact me.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  ></input>
+                </li>
+                <li>
+                  <textarea
+                    name="message"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
