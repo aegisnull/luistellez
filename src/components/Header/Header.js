c2b7671ff39ccf5717e7b73d04411707e935326d
Luis Tellez <luis@luistellez.com>
2023-01-11 23:35:30 -0800

    feat: finish HTML markup for Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,27 @@ import Logo from '../../images/logo.svg';
 
 function Header() {
   return (
-    <header>
-      <h1>React Router</h1>
-      <img src={Logo} alt='React Router Logo' />
+    <header className='header'>
+      <div className='header__container'>
+        <img src={Logo} alt='Logo' />
+        <nav className='header__nav'>
+          <li className='header__nav_link'>
+            <a href='#home'>home</a>
+          </li>
+
+          <li className='header__nav_link'>
+            <a href='#works'>works</a>
+          </li>
+
+          <li className='header__nav_link'>
+            <a href='#about-me'>about-me</a>
+          </li>
+
+          <li className='header__nav_link'>
+            <a href='#contact'>contact</a>
+          </li>
+        </nav>
+      </div>
     </header>
   );
 }
