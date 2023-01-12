c0dace6e853b80fe32e263d09d90fec874011762
Luis Tellez <luis@luistellez.com>
2023-01-12 09:53:46 -0800

    style: set max-width to be set globally at .App CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,26 +4,24 @@ import Logo from '../../images/logo.svg';
 function Header() {
   return (
     <header className='header'>
-      <div className='header__container'>
-        <img src={Logo} alt='Logo' />
-        <nav className='header__nav'>
-          <li className='header__nav_link'>
-            <a href='#home'>home</a>
-          </li>
+      <img src={Logo} alt='Logo' />
+      <nav className='header__nav'>
+        <li className='header__nav_link'>
+          <a href='#home'>home</a>
+        </li>
 
-          <li className='header__nav_link'>
-            <a href='#works'>works</a>
-          </li>
+        <li className='header__nav_link'>
+          <a href='#works'>works</a>
+        </li>
 
-          <li className='header__nav_link'>
-            <a href='#about-me'>about-me</a>
-          </li>
+        <li className='header__nav_link'>
+          <a href='#about-me'>about-me</a>
+        </li>
 
-          <li className='header__nav_link'>
-            <a href='#contact'>contact</a>
-          </li>
-        </nav>
-      </div>
+        <li className='header__nav_link'>
+          <a href='#contact'>contact</a>
+        </li>
+      </nav>
     </header>
   );
 }
