4f705eedd50113b7f959187474eeb4a8570fb816
Luis Tellez <luis@luistellez.com>
2023-01-11 23:13:30 -0800

    feat: add colors as root variables in CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import './Header.scss';
+import Logo from '../../images/logo.svg';
 
 function Header() {
   return (
     <header>
       <h1>React Router</h1>
+      <img src={Logo} alt='React Router Logo' />
     </header>
   );
 }
