bea41316b25747fa1acea5ce153d0f7e789399f6
Luis Tellez <luis@luistellez.com>
2023-01-12 18:12:17 -0800

    feat: init Home component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import './Home.scss';
+
+function Home() {
+  return (
+    <section className='home'>
+      <h1 className='home__title'>
+        Hi, I&aposm Luis Tellez a <span className='home__title_accent'>web developer</span> and
+        <span className='home__title_accent'>UI/UX Enthusiast</span>
+      </h1>
+    </section>
+  );
+}
+
+export default Home;
