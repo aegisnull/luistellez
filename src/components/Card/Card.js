9ef2614d91ce2f25d18fc49b242c2847e9f7313c
Luis Tellez <luis@luistellez.com>
2023-01-13 11:43:17 -0800

    feat: finish HTML markup for Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,22 +3,17 @@ import './Card.scss';
 function Card() {
   return (
     <article className='card'>
-      <div className='card__img'>
-        <img src='https://via.placeholder.com/300x200' alt='project' />
+      <img src='https://via.placeholder.com/330x201' alt='project' className='card__image' />
+      <div className='card__tags'>
+        <span className='card__tag'>HTML</span>
+        <span className='card__tag'>CSS</span>
+        <span className='card__tag'>JavaScript</span>
       </div>
       <div className='card__content'>
-        <h3 className='card__title'>Project title</h3>
-        <p className='card__text'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl vel ultricies
-          lacinia, nunc nisl ultricies nisl, nec ultricies nisl nisl sit amet nisl. Sed euismod,
-          nisl vel ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nisl sit amet
-          nisl.
-        </p>
-        <div className='card__tags'>
-          <span className='card__tag'>HTML</span>
-          <span className='card__tag'>CSS</span>
-          <span className='card__tag'>JavaScript</span>
-        </div>
+        <h3 className='card__title'>NewsExplorer</h3>
+        <p className='card__description'>News Reader App</p>
+        <div className='button_accent'>View Project</div>
+        <div className='button'>View Code</div>
       </div>
     </article>
   );
