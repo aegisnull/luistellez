35b4917e7378a6ac5bb045521a2aea695f8a2783
Luis Tellez <luis@luistellez.com>
2023-01-13 00:32:55 -0800

    feat: add Roboto-Slab medium font

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -3,7 +3,7 @@ import './Quote.scss';
 function Quote() {
   return (
     <section className='quote'>
-      <p className='quote__text'>Simplicity is the soul of efficiency</p>
+      <q className='quote__text'>Simplicity is the soul of efficiency</q>
       <p className='quote__author'>- Austin Freeman</p>
     </section>
   );
