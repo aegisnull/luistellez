ce2e1aa4e6c4a0b1ae95310e4fff51e3bd935586
Luis Tellez <luis@luistellez.com>
2023-01-13 00:53:30 -0800

    feat: init Projects component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import Home from '../Home/Home';
 import Quote from '../Quote/Quote';
+import Projects from '../Projects/Projects';
 
 function Main() {
   return (
     <main>
       <Home />
       <Quote />
+      <Projects />
     </main>
   );
 }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import './Projects.scss';
+
+function Projects() {
+  return (
+    <section className='Projects'>
+      <h2>Projects</h2>
+    </section>
+  );
+}
+
+export default Projects;
