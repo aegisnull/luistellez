4749e6cfe7b864d1897e822b731f257603bc99c3
Luis Tellez <luis@luistellez.com>
2023-01-13 11:33:43 -0800

    style: add bottom padding to Projects title container

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,13 @@ import './Projects.scss';
 function Projects() {
   return (
     <section className='projects'>
-      <div className='projects__title'>
-        <h2 className='title'>projects</h2>
-        <div className='projects__line' />
+      <div className='projects__container'>
+        <div className='projects__title'>
+          <h2 className='title'>projects</h2>
+          <div className='projects__line' />
+        </div>
+        <div className='projects__nav'>View all</div>
       </div>
-      <div className='projects__nav'>View all</div>
     </section>
   );
 }
