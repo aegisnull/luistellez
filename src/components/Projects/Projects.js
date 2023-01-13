cae496004765ac4b1f214ca0bbd21d7f5f89dad5
Luis Tellez <luis@luistellez.com>
2023-01-13 01:11:23 -0800

    style: add reusable title element with varying line

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,11 @@ import './Projects.scss';
 
 function Projects() {
   return (
-    <section className='Projects'>
-      <h2>Projects</h2>
+    <section className='projects'>
+      <div className='projects__title'>
+        <h2 className='title'>projects</h2>
+        <div className='projects__line' />
+      </div>
     </section>
   );
 }
