52ba1e4f79520be5ac79db397c976bf8041ddb96
Luis Tellez <luis@luistellez.com>
2023-01-21 15:15:52 -0800

    refactor: improve Skills component readability by decoupling hard coded skills into separate Skills.js

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,6 @@
 import './Skills.scss';
 import DecorativeImage from '../../images/skills.svg';
+import skills from '../../utils/Skills';
 
 function Skills() {
   return (
@@ -13,50 +14,16 @@ function Skills() {
       <div className='skills__container_content'>
         <img src={DecorativeImage} alt='skills' className='skills__image' />
         <div className='skills__card-container'>
-          <div className='skills__card'>
-            <h3 className='skills__card-title'>Languages</h3>
-            <ul className='skills__card-list'>
-              <li className='skills__card-item'>JavaScript</li>
-              <li className='skills__card-item'>TypeScript</li>
-            </ul>
-          </div>
-          <div className='skills__card'>
-            <h3 className='skills__card-title'>Databases</h3>
-            <ul className='skills__card-list'>
-              <li className='skills__card-item'>MongoDB</li>
-              <li className='skills__card-item'>MySQL</li>
-            </ul>
-          </div>
-          <div className='skills__card'>
-            <h3 className='skills__card-title'>Tools</h3>
-            <ul className='skills__card-list'>
-              <li className='skills__card-item'>Git</li>
-              <li className='skills__card-item'>GitHub</li>
-              <li className='skills__card-item'>VS Code</li>
-              <li className='skills__card-item'>Postman</li>
-              <li className='skills__card-item'>Figma</li>
-              <li className='skills__card-item'>Photoshop</li>
-              <li className='skills__card-item'>Cyberduck</li>
-            </ul>
-          </div>
-          <div className='skills__card'>
-            <h3 className='skills__card-title'>Other</h3>
-            <ul className='skills__card-list'>
-              <li className='skills__card-item'>HTML</li>
-              <li className='skills__card-item'>CSS</li>
-              <li className='skills__card-item'>SASS</li>
-              <li className='skills__card-item'>REST</li>
-            </ul>
-          </div>
-
-          <div className='skills__card'>
-            <h3 className='skills__card-title'>Frameworks</h3>
-            <ul className='skills__card-list'>
-              <li className='skills__card-item'>React</li>
-              <li className='skills__card-item'>Express</li>
-              <li className='skills__card-item'>Node</li>
-            </ul>
-          </div>
+          {skills.map((skill) => (
+            <div className='skills__card'>
+              <h3 className='skills__card-title'>{skill.title}</h3>
+              <ul className='skills__card-list'>
+                {skill.items.map((item) => (
+                  <li className='skills__card-item'>{item}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/utils/Skills.js
@@ -0,0 +1,23 @@
+const skills = [
+  {
+    title: 'Languages',
+    items: ['JavaScript'],
+  },
+  {
+    title: 'Databases',
+    items: ['MongoDB', 'MySQL'],
+  },
+  {
+    title: 'Tools',
+    items: ['Git', 'GitHub', 'VS Code', 'Postman', 'Figma', 'Photoshop', 'Cyberduck'],
+  },
+  {
+    title: 'Other',
+    items: ['HTML', 'CSS', 'SASS', 'REST'],
+  },
+  {
+    title: 'Frameworks',
+    items: ['React', 'Express', 'Node'],
+  },
+];
+export default skills;
