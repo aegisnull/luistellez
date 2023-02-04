892fbe1518968df44bbf5f5d7fbd0fc30d92ea66
Luis Tellez <luis@luistellez.com>
2023-02-04 12:04:32 -0800

    fix: adjust setting to use Markdown locally instead of importing from external sources

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,16 +1,15 @@
 import './Post.scss';
 import React from 'react';
 import Markdown from 'markdown-to-jsx';
-
 function Post() {
   const [post, setPost] = React.useState('');
 
   React.useEffect(() => {
-    fetch(
-      'https://raw.githubusercontent.com/aegisnull/luistellez/feat/blog/src/posts/hello-world.md',
-    )
-      .then((res) => res.text())
-      .then((text) => setPost(text));
+    import('../../posts/hello-world.md').then((res) => {
+      fetch(res.default)
+        .then((response) => response.text())
+        .then((text) => setPost(text));
+    });
   }, []);
 
   return (
