00681ef4ccbd224a6ac2ddf5b3fdf66b9fb3da73
Luis Tellez <luis@luistellez.com>
2023-02-04 21:39:15 -0800

    feat: set functions to countWords, fint the title, and remove the title from Markdown

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -10,9 +10,41 @@ function Post() {
     import('../../posts/hello-world.md').then((res) => {
       fetch(res.default)
         .then((response) => response.text())
-        .then((text) => setPost(text));
+        .then((text) => {
+          const cleanedText = removeMetadata(text);
+          const title = findTitle(cleanedText);
+          const postWithoutTitle = removeTitle(cleanedText, title);
+          setPost(postWithoutTitle);
+        });
     });
   }, []);
+
+  // function that removes the metadata from the markdown file imported
+  function removeMetadata(markdown) {
+    const startIndex = markdown.indexOf('---');
+    const endIndex = markdown.indexOf('---', startIndex + 3);
+    return markdown.slice(0, startIndex) + markdown.slice(endIndex + 3);
+  }
+
+  // function that counts the total words in the markdown file imported, this is to then calculate the reading time
+  function countWords(str) {
+    return str.split(' ').length;
+  }
+
+  // function that finds the title of the markdown file imported it checks for the first # and then returns the text after it removing the #
+  function findTitle(markdown) {
+    let match = markdown.match(/^#[^\n]+/m);
+    return match ? match[0].slice(1) : null;
+  }
+
+  function removeTitle(markdown, title) {
+    const titleIndex = markdown.indexOf(title);
+    const endOfLineIndex = markdown.indexOf('\n', titleIndex);
+    return markdown.slice(0, titleIndex) + markdown.slice(endOfLineIndex + 1);
+  }
+
+  console.log(countWords(post));
+  console.log(findTitle(post));
 
   return (
     <article className='post'>
