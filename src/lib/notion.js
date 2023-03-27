f8a4a1298004087df95e1a0b3a01284edadd5ad8
Luis Tellez <luis@luistellez.com>
2023-03-26 21:00:00 -0700

    🔨 refactor(notion.js): add support for nested bulleted and numbered lists This commit refactors the getBlocks function to support nested bulleted and numbered lists. The function now fetches all child blocks recursively and maps them to a new array of blocks. The new array is then reduced to a final array of blocks, which includes the nested bulleted and numbered lists. The getRandomInt function is also added to generate a random integer for the id of the new bulleted and numbered lists.
    ✨ feat([id].js): add support for rendering Notion database pages
    This commit adds support for rendering Notion database pages. The `Post` component is responsible for rendering the page and its blocks. The `Text` component is used to render rich text. The `renderBlock` function is used to render different types of blocks. The `renderNestedList` function is used to render nested lists. The `getStaticPaths` function is used to generate static paths for all pages in the database. The `getStaticProps` function is used to fetch the page and its blocks.
    
    💄 style(index.module.css): add styles for index page
    This commit adds styles for the index page. It includes styles for the container, logos, header, plus, heading, posts, and post. The styles are responsive and include media queries for dark mode.
    
    🎨 style(post.module.css): add styles for post page
    This commit adds styles for the post page. The styles include a container with a max-width of 700px, h1, img, figure, figcaption, name, back, bold, code, italic, pre, code_block, file, strikethrough, underline, bookmark, table, row, and childPage. The media query for prefers-color-scheme: dark is also included.

--- src/lib/notion.js
+++ src/lib/notion.js
@@ -17,9 +17,55 @@ export const getPage = async (pageId) => {
 };
 
 export const getBlocks = async (blockId) => {
-  const response = await notion.blocks.children.list({
+  blockId = blockId.replaceAll("-", "");
+
+  const { results } = await notion.blocks.children.list({
     block_id: blockId,
-    page_size: 50,
+    page_size: 100,
   });
-  return response.results;
+
+  // Fetches all child blocks recursively - be mindful of rate limits if you have large amounts of nested blocks
+  // See https://developers.notion.com/docs/working-with-page-content#reading-nested-blocks
+  const childBlocks = results.map(async (block) => {
+    if (block.has_children) {
+      const children = await getBlocks(block.id);
+      return { ...block, children };
+    }
+    return block;
+  });
+
+  return await Promise.all(childBlocks).then((blocks) => {
+    return blocks.reduce((acc, curr) => {
+      if (curr.type === "bulleted_list_item") {
+        if (acc[acc.length - 1]?.type === "bulleted_list") {
+          acc[acc.length - 1][acc[acc.length - 1].type].children?.push(curr);
+        } else {
+          acc.push({
+            id: getRandomInt(10 ** 99, 10 ** 100).toString(),
+            type: "bulleted_list",
+            bulleted_list: { children: [curr] },
+          });
+        }
+      } else if (curr.type === "numbered_list_item") {
+        if (acc[acc.length - 1]?.type === "numbered_list") {
+          acc[acc.length - 1][acc[acc.length - 1].type].children?.push(curr);
+        } else {
+          acc.push({
+            id: getRandomInt(10 ** 99, 10 ** 100).toString(),
+            type: "numbered_list",
+            numbered_list: { children: [curr] },
+          });
+        }
+      } else {
+        acc.push(curr);
+      }
+      return acc;
+    }, []);
+  });
 };
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
