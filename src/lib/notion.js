558dcbf5562bcc9fe1de9cfa813ccf24d5ce6877
Luis Tellez <luis@luistellez.com>
2023-03-27 08:43:34 -0700

    🚀 feat(package.json): add dependencies for Notion API and markdown conversion ✨ feat(notion.js): add function to get single blog post by slug The package.json file has been updated to include dependencies for Notion API and markdown conversion. The notion.js file has been updated to include a new function that retrieves a single blog post by its slug. The function uses the Notion API to query the database for the post with the matching slug, and then converts the retrieved data to markdown using the NotionToMarkdown library. The function returns an object containing the post metadata and the markdown content.
    🗑️ chore([id].js): delete unused code
    The file [id].js was deleted and all its contents were removed. The code was no longer being used and was therefore deleted to improve the codebase's cleanliness.
    
    🚀 feat(blog.js): add blog posts to the blog page
    This commit adds the functionality to display blog posts on the blog page. The `getAllPublished` function is imported from the `notion` library to fetch all published posts. The `BlogPage` component now receives a `posts` prop, which is an array of post objects. The posts are then mapped over and displayed on the page. The `getStaticProps` function is also added to fetch the posts data at build time and pass it as a prop to the `BlogPage` component.
    
    🆕 feat([slug].js): add support for syntax highlighting in blog posts
    This commit adds support for syntax highlighting in blog posts. The `react-syntax-highlighter` package is used to highlight code blocks in the markdown. The `CodeBlock` component is created to render the highlighted code. The `getStaticProps` and `getStaticPaths` functions are also added to support static generation of blog posts.
    
    💄 style(Home.module.css): add styles for card hover effect
    This commit adds styles for the hover effect on the card element. When the user hovers over the card, the border color changes to blue. The card element also has a link with a border that changes color on hover. The styles for the card element are now in a separate file named Home.module.css.

--- src/lib/notion.js
+++ src/lib/notion.js
@@ -20,23 +20,18 @@ export const getAllPublished = async () => {
       },
     ],
   });
-
   const allPosts = posts.results;
-
   return allPosts.map((post) => {
     return getPageMetaData(post);
   });
 };
-
 const getPageMetaData = (post) => {
   const getTags = (tags) => {
     const allTags = tags.map((tag) => {
       return tag.name;
     });
-
     return allTags;
   };
-
   return {
     id: post.id,
     title: post.properties.Name.title[0].plain_text,
@@ -62,17 +57,38 @@ function getToday(datestring) {
     "November",
     "December",
   ];
-
   let date = new Date();
-
   if (datestring) {
     date = new Date(datestring);
   }
-
   const day = date.getDate();
   const month = months[date.getMonth()];
   const year = date.getFullYear();
   let today = `${month} ${day}, ${year}`;
-
   return today;
 }
+
+const { NotionToMarkdown } = require("notion-to-md");
+const n2m = new NotionToMarkdown({ notionClient: notion });
+
+export const getSingleBlogPostBySlug = async (slug) => {
+  const response = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+    filter: {
+      property: "Slug",
+      formula: {
+        string: {
+          equals: slug,
+        },
+      },
+    },
+  });
+  const page = response.results[0];
+  const metadata = getPageMetaData(page);
+  const mdblocks = await n2m.pageToMarkdown(page.id);
+  const mdString = n2m.toMarkdownString(mdblocks);
+  return {
+    metadata,
+    markdown: mdString,
+  };
+};
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,34 @@
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
-function BlogPage() {
+import Link from "next/link";
+import { getAllPublished } from "../lib/notion";
+import styles from "../styles/Home.module.css";
+
+function BlogPage({ posts }) {
+  if (!posts) return <h1>No posts</h1>;
   return (
     <>
       <div className="App">
         <Header />
-        <h1>Blog</h1>
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <h1>Blog</h1>
+            {posts.map((post, index) => (
+              <section key={index} className={styles.card}>
+                <div>
+                  <h2>
+                    <Link href={`/posts/${post.slug}`} legacyBehavior>
+                      <a className={styles.action}>{post.title}</a>
+                    </Link>
+                  </h2>
+                  <div>{post.date}</div>
+                </div>
+                <p>{post.description}</p>
+              </section>
+            ))}
+          </main>
+        </div>
         <Footer />
       </div>
     </>
@@ -14,3 +36,13 @@ function BlogPage() {
 }
 
 export default BlogPage;
+
+export const getStaticProps = async () => {
+  const data = await getAllPublished();
+  return {
+    props: {
+      posts: data,
+    },
+    revalidate: 60,
+  };
+};
