61dbbab4139f0a7d70958054f55ebdc07b48d132
Luis Tellez <luis@luistellez.com>
2023-03-28 14:00:02 -0700

    🎨 style(Blog.jsx): change Blog.scss to Blog.module.scss and update styles ✨ feat(Blog.jsx): add conditional rendering for no posts The Blog component now uses CSS modules instead of a global stylesheet. The styles have been updated to reflect this change. The component now also has conditional rendering for when there are no posts to display.
    🎨 style(Blog.module.scss): add styles for blog cards
    This commit adds styles for the blog cards, including the container, main, card, cards__container, blog__title, blog__line, card__tags, card__tag, card__title, card__content, card__postdate, and card__description. These styles improve the visual appearance of the blog cards and make them more consistent with the overall design of the application.
    
    🎨 style(blog.js): refactor BlogPage component to use Blog component
    The BlogPage component has been refactored to use the Blog component, which improves code readability and reduces the amount of code needed to render the blog page. The Head component has been moved to the top of the component for better organization.

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,40 @@
-import './Blog.scss';
+import styles from './Blog.module.scss';
+import Link from 'next/link';
 
-function Blog() {
+function Blog({ posts }) {
+  if (!posts) return <h1>No posts</h1>;
   return (
-    <div className='blog'>
-      <h1>Blog</h1>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div className={styles.blog__title}>
+          <h2 className="title">Blog</h2>
+          <div className={styles.blog__line} />
+        </div>
+        <div className={styles.cards__container}>
+          {posts.map((post, index) => (
+            <article key={index} className={styles.card}>
+              <img className={styles.card__image} width="328px" height="200px" src={post.cover} />
+              <div className={styles.card__tags}>
+                {post.tags.map((tag, index) => (
+                  <span key={index} className={styles.card__tag}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <div className={styles.card__content}>
+                <div className={styles.card__postdate}>{post.date}</div>
+                <h2>
+                  <Link href={`/blog/${post.slug}`} legacyBehavior>
+                    <a className={styles.card__title}>{post.title}</a>
+                  </Link>
+                </h2>
+
+                <p className={styles.card__description}>{post.description}</p>
+              </div>
+            </article>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,65 +1,24 @@
+import Blog from '@/components/Blog/Blog';
 import Head from 'next/head';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
-
-import Link from 'next/link';
-import { getAllPublished } from '../lib/notion';
-import styles from '../styles/Home.module.css';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import { getAllPublished } from '@/lib/notion';
 
 function BlogPage({ posts }) {
-  if (!posts) return <h1>No posts</h1>;
   return (
-    <>
-      <div className="App">
-        <Head>
-          <title>Luis Tellez | Blog</title>
-          <meta
-            name="description"
-            content="Looking for insights and expertise on the latest developments in web development and software engineering? Check out the blog page of my developer portfolio for informative and engaging content. Stay ahead of the curve and take your digital projects to the next level with my tips and tutorials."
-          />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <Header />
-        <div className={styles.container}>
-          <main className={styles.main}>
-            <div className={styles.blog__title}>
-              <h2 className="title">Blog</h2>
-              <div className={styles.blog__line} />
-            </div>
-            <div className={styles.cards__container}>
-              {posts.map((post, index) => (
-                <article key={index} className={styles.card}>
-                  <img
-                    className={styles.card__image}
-                    width="328px"
-                    height="200px"
-                    src={post.cover}
-                  />
-                  <div className={styles.card__tags}>
-                    {post.tags.map((tag, index) => (
-                      <span key={index} className={styles.card__tag}>
-                        {tag}
-                      </span>
-                    ))}
-                  </div>
-                  <div className={styles.card__content}>
-                    <div className={styles.card__postdate}>{post.date}</div>
-                    <h2>
-                      <Link href={`/blog/${post.slug}`} legacyBehavior>
-                        <a className={styles.card__title}>{post.title}</a>
-                      </Link>
-                    </h2>
-
-                    <p className={styles.card__description}>{post.description}</p>
-                  </div>
-                </article>
-              ))}
-            </div>
-          </main>
-        </div>
-        <Footer />
-      </div>
-    </>
+    <div className="App">
+      <Header />
+      <Head>
+        <title>Luis Tellez | Blog</title>
+        <meta
+          name="description"
+          content="Looking for insights and expertise on the latest developments in web development and software engineering? Check out the blog page of my developer portfolio for informative and engaging content. Stay ahead of the curve and take your digital projects to the next level with my tips and tutorials."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Blog posts={posts} />
+      <Footer />
+    </div>
   );
 }
 
