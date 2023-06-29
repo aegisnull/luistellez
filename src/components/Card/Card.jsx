0744a456d2c12b581e139a2fe3ac932837368d90
Luis Tellez <luis@luistellez.com>
2023-06-28 20:41:29 -0700

    🐛 fix(Card.jsx): fix import statement to use single quotes for consistency 🎨 style(Projects.module.scss): add justify-content: center to projects container for better alignment 🎨 style(Projects.module.scss): adjust margin for projects button for better spacing 🎨 style(Projects.module.scss): add padding-bottom to projects container for better spacing on tablet screens 🎨 style(Projects.module.scss): add padding-top and padding-bottom to projects container for better spacing on mobile screens 🎨 style(Projects.module.scss): adjust width and max-width of projects line for better responsiveness on tablet screens

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,9 @@
-import styles from "./Card.module.scss";
+import styles from './Card.module.scss';
 
 function Card({ image, tags, title, description, live, github }) {
   return (
     <article className={styles.card}>
-      <video
-        autoPlay
-        muted
-        loop
-        className={styles.card__image}
-        width="328px"
-        height="200px"
-      >
+      <video autoPlay muted loop className={styles.card__image} width="99.5%" height="auto">
         <source src={image} type="video/webm" />
       </video>
       <div className={styles.card__tags}>
@@ -24,21 +17,11 @@ function Card({ image, tags, title, description, live, github }) {
         <h3 className={styles.card__title}>{title}</h3>
         <p className={styles.card__description}>{description}</p>
         <div className={styles.card__buttons}>
-          <a
-            href={live}
-            className="button_accent"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href={live} className="button_accent" target="_blank" rel="noopener noreferrer">
             <div className="button__text card__button_live">Live</div>
           </a>
           {github && (
-            <a
-              href={github}
-              className="button"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href={github} className="button" target="_blank" rel="noopener noreferrer">
               <div className="button__text card__button_git">Github</div>
             </a>
           )}
