7cf10557e050f605bc71f6235836287d2434f2f7
Luis Tellez <luis@luistellez.com>
2023-06-30 11:54:47 -0700

    feat: add new projects [General Store Frontend, General Store Backend, React Calendar]

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      'user-images.githubusercontent.com',
-      'camo.githubusercontent.com',
-      'github.com',
-      'private-user-images.githubusercontent.com',
-    ],
-  },
 };
 
 module.exports = nextConfig;
--- src/utils/Projects.js
+++ src/utils/Projects.js
@@ -1,8 +1,34 @@
 const cardData = [
   {
     id: 1,
-    image:
-      'https://user-images.githubusercontent.com/27663011/212429268-38a790be-a69f-4219-b336-2d709a7038b9.gif',
+    image: '/images/projects/GeneralStoreFrontend.webp',
+    tags: ['NextJS', 'MongoDB', 'Jest/RTL', 'Cypress'],
+    title: 'General Store Frontend',
+    description: 'eCommerce App Frontend',
+    live: 'https://store-frontend-aegisnull.vercel.app',
+    github: 'https://github.com/aegisnull/general-store-frontend',
+  },
+  {
+    id: 2,
+    image: '/images/projects/GeneralStoreBackend.webp',
+    tags: ['ExpressJS', 'MongoDB', 'K6'],
+    title: 'General Store Backend',
+    description: 'eCommerce App Backend',
+    live: 'http://news-explorer.projects.luistellez.com',
+    github: 'https://github.com/aegisnull/general-store-backend',
+  },
+  {
+    id: 3,
+    image: '/images/projects/ReactCalendar.webp',
+    tags: ['VitéJS', 'JS Date'],
+    title: 'React Calendar',
+    description: 'React Calendar App',
+    live: 'https://react-calendar-altomobile.netlify.app',
+    github: 'https://github.com/aegisnull/react-calendar',
+  },
+  {
+    id: 4,
+    image: '/images/projects/NewsExplorer.webp',
     tags: ['React', 'NodeJS', 'ExpressJS', 'MongoDB'],
     title: 'News Explorer',
     description: 'News Reader Full Stack App',
@@ -10,9 +36,8 @@ const cardData = [
     github: 'https://github.com/aegisnull/news-explorer-frontend',
   },
   {
-    id: 2,
-    image:
-      'https://camo.githubusercontent.com/11f092ff9112bb0d096bb3f11331ce72976dc2623c96d57a255365470f4b8ad9/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f464c38324852634f7352383239704e4e51762f67697068792e676966',
+    id: 5,
+    image: '/images/projects/Moody.webp',
     tags: ['HTML', 'CSS', 'JavaScript'],
     title: 'Moody',
     description: 'Hackathon Project Landing Page',
@@ -20,9 +45,8 @@ const cardData = [
     github: 'https://github.com/aegisnull/moody',
   },
   {
-    id: 3,
-    image:
-      'https://github.com/aegisnull/react-around-api-full_es/assets/27663011/b8f431c5-49c0-4b16-ad33-9a25f34032ee',
+    id: 6,
+    image: '/images/projects/AroundTheUS.webp',
     tags: ['React', 'NodeJS', 'ExpressJS', 'MongoDB'],
     title: 'Around The U.S.',
     description: 'Instagram like app',
@@ -30,36 +54,32 @@ const cardData = [
     github: 'https://github.com/aegisnull/react-around-api-full_es',
   },
   {
-    id: 4,
-    image:
-      'https://private-user-images.githubusercontent.com/27663011/250006518-039d88a9-12af-4db3-8dae-ba995b36e6a0.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJrZXkxIiwiZXhwIjoxNjg4MTQ2MDY5LCJuYmYiOjE2ODgxNDU3NjksInBhdGgiOiIvMjc2NjMwMTEvMjUwMDA2NTE4LTAzOWQ4OGE5LTEyYWYtNGRiMy04ZGFlLWJhOTk1YjM2ZTZhMC5naWY_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMwNjMwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMDYzMFQxNzIyNDlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xNGFiZjkyYTJhYzQ4NzczNTI0NTlmODgyOWYxMDMyNmJhNzFjOGMxNzhiYmU4YzUyMTU3YjdjM2U5NDBhMDhjJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.iMqYFet06-hGctSnlzKo2DC7I19DVEZPidllURh6BFo',
+    id: 7,
+    image: '/images/projects/GMG.webp',
     tags: ['WordPress', 'PHP', 'HTML', 'CSS'],
     title: 'GMG Manufacturing',
     description: 'Local Manufacturing  Website',
     live: 'https://web.archive.org/web/20221208215442/https://gmgmanufacturing.com/',
   },
   {
-    id: 5,
-    image:
-      'https://private-user-images.githubusercontent.com/27663011/250007044-01082e82-0433-44ec-8633-d51b6befb431.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJrZXkxIiwiZXhwIjoxNjg4MTQ2MDMyLCJuYmYiOjE2ODgxNDU3MzIsInBhdGgiOiIvMjc2NjMwMTEvMjUwMDA3MDQ0LTAxMDgyZTgyLTA0MzMtNDRlYy04NjMzLWQ1MWI2YmVmYjQzMS5naWY_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMwNjMwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMDYzMFQxNzIyMTJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02MDkxMTg2YTlkMjJiMzdjZGJjZjk0ZjJkMzg4YWMxMWM3ODAyMjU5NmQ5YTM4ODU0MGZlZTRjYzhkODJlMDAzJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.dWXfZ8mK2ce79T2Eoh-wBDayV7CT3ykozAxt31X-97g',
+    id: 8,
+    image: '/images/projects/CirculoCafe.webp',
     tags: ['HTML', 'CSS', 'jQuery'],
     title: 'Circulo Café',
     description: 'Coffee Shop Website',
     live: 'https://circulocafe.com',
   },
   {
-    id: 6,
-    image:
-      'https://github.com/aegisnull/prepara-tu-conta/assets/27663011/e5d2fcee-dcd5-4853-9e3d-b594a9017cdf',
+    id: 9,
+    image: '/images/projects/PreparaTuConta.webp',
     tags: ['HTML', 'CSS', 'jQuery'],
     title: 'Prepara tu Conta',
     description: 'Accounting Website',
     live: 'https://aegisnull.github.io/prepara-tu-conta/',
   },
   {
-    id: 7,
-    image:
-      'https://github.com/aegisnull/practicum-art-gallery/assets/27663011/871f2efe-e37d-4327-b8d3-b91735791d48',
+    id: 10,
+    image: '/images/projects/PatriaAPatria.webp',
     tags: ['HTML', 'CSS', 'CSS Grid'],
     title: 'Practicum Art Gallery',
     description: 'Art Gallery Website',
@@ -67,9 +87,8 @@ const cardData = [
     github: 'https://github.com/aegisnull/practicum-art-gallery',
   },
   {
-    id: 8,
-    image:
-      'https://github.com/aegisnull/aprender-a-aprender/assets/27663011/c5e9f257-75f0-4b2e-9d53-74df9b206627',
+    id: 11,
+    image: '/images/projects/AprenderAAprender.webp',
     tags: ['HTML', 'CSS', 'BEM'],
     title: 'Aprender a Aprender',
     description: 'Learning Info Website',
@@ -77,9 +96,8 @@ const cardData = [
     github: 'https://github.com/aegisnull/aprender-a-aprender',
   },
   {
-    id: 9,
-    image:
-      'https://github.com/aegisnull/memorama/assets/27663011/7fcc9d01-3f21-49c0-ad76-7e7aa6df5768',
+    id: 12,
+    image: '/images/projects/Memorama.webp',
     tags: ['HTML', 'CSS', 'JavaScript'],
     title: 'Memorama Game',
     description: 'Memory Game Website',
@@ -87,18 +105,16 @@ const cardData = [
     github: 'https://github.com/aegisnull/memorama',
   },
   {
-    id: 10,
-    image:
-      'https://private-user-images.githubusercontent.com/27663011/250008214-0e0cc633-206a-44c4-977c-931ce16de248.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJrZXkxIiwiZXhwIjoxNjg4MTQ2MTAxLCJuYmYiOjE2ODgxNDU4MDEsInBhdGgiOiIvMjc2NjMwMTEvMjUwMDA4MjE0LTBlMGNjNjMzLTIwNmEtNDRjNC05NzdjLTkzMWNlMTZkZTI0OC5naWY_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMwNjMwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMDYzMFQxNzIzMjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1mMDAzMGQwNDg0MjZlNzA5NTlkZGUzMmFiNmFlM2EwYTJiNzhmMWE2OTU0MjhlNjRlOTgxMjAyNzA0ZjU1ODhhJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.afQIDpnppPUyfJWP74vSnzqo137LLUNzA0A5piEV_IM',
+    id: 13,
+    image: '/images/projects/Intelegal.webp',
     tags: ['WordPress', 'PHP', 'HTML', 'CSS'],
     title: 'Intelegal',
     description: 'Website for Law Firm',
     live: 'https://intelegal.com.mx',
   },
   {
-    id: 11,
-    image:
-      'https://github.com/aegisnull/odin-landing-page/assets/27663011/6cd6ac23-bb35-4f02-bc07-ce34838c55b3',
+    id: 14,
+    image: '/images/projects/OdinLandingPage.webp',
     tags: ['HTML', 'CSS'],
     title: 'Odin Landing Page',
     description: 'Simple Landing Page',
@@ -106,9 +122,8 @@ const cardData = [
     github: 'https://github.com/aegisnull/odin-landing-page',
   },
   {
-    id: 12,
-    image:
-      'https://private-user-images.githubusercontent.com/27663011/250008748-fd556730-3fa8-4c36-89da-5ffd2ac9f948.gif?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJrZXkxIiwiZXhwIjoxNjg4MTQ2MTAxLCJuYmYiOjE2ODgxNDU4MDEsInBhdGgiOiIvMjc2NjMwMTEvMjUwMDA4NzQ4LWZkNTU2NzMwLTNmYTgtNGMzNi04OWRhLTVmZmQyYWM5Zjk0OC5naWY_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMwNjMwJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMDYzMFQxNzIzMjFaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0zY2YzN2JiNDI2NDUxMjQyOTMyMGI1YTJlYTUyZWQ2NDgyNmU2NzQ2YTFiM2NlNTMwMDA5ZDE2ZjBhZmU2ZTNjJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.CdCxWw5wjVbQmHnczMuot8asq9NTH2G0QIo4v7_YcHM',
+    id: 15,
+    image: '/images/projects/IntelegalLandingPage.webp',
     tags: ['WordPress', 'PHP', 'HTML', 'CSS'],
     title: 'Intelegal Landing Page',
     description: 'Landing Page for Real Estate',
